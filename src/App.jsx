3635f8d204c692d1e85ece921048669f7fcb6459
Ankita <Ankita012@users.noreply.github.com>
2024-01-10 00:35:10 +1300

    Chatgpt with react js

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer,MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
+
+const API_KEY = "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";
+const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
+  "role": "system", "content": "Explain things like you're talking to a software professional with 3 years of experience."
+}
+function App() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am Chatgpt!",
+      sender: "chatGPT"
+    }
+  ])
+  const handleSend = async (message) =>{
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+    setTyping(true);
+    await processMessageToChatGPT(newMessages);
+  }
+
+  async function processMessageToChatGPT(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject)=>{
+      let role="";
+      if(messageObject.sender == "ChatGPT") {
+        role = "assistant";
+      }
+      else {
+        role = "user";
+      }
+      return {role: role, content: messageObject.message}
+    });
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessages
+      ]
+    }
+    await fetch("https://api.openai.com/v1/chat/completions",{
+      method:"POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data)=>{
+      return data.json();
+    }).then((data)=>{
+      console.log(data);
+      console.log(data.choices[0].message.content);
+      setMessages([
+        ...chatMessages,{
+          message: data.choices[0].message.content,
+          sender: "ChatGPT" 
+        }
+      ]);
+      setTyping(false);
+    })
+  } 
+
+  return (
+    <div className="App">
+      <div style={{position: "relative", height: "800px", width: "700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList 
+              scrollBehavior="smooth"
+              typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing"/> : null}>
+              {messages.map((message,i)=>{
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  )
+}
+
+export default App
